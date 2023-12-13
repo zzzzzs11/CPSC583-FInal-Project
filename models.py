0b7d5eaf1af22cea71452dd18324c713ea19ee9f
zzzzzs11 <73007698+zzzzzs11@users.noreply.github.com>
2023-12-12 23:31:34 -0500

    Add files via upload
    some interesting models

---
+++ models.py
@@ -0,0 +1,243 @@
+#https://github.com/Diego999/pyGAT/blob/master/train.py#L55
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from layers import GraphAttentionLayer, SpGraphAttentionLayer
+from torch_geometric.nn import SAGEConv
+
+import math
+import numpy as np
+from torch.nn.parameter import Parameter
+
+
+class GAT(nn.Module):
+    def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads):
+        """Dense version of GAT."""
+        super(GAT, self).__init__()
+        self.dropout = dropout
+
+        self.attentions = [GraphAttentionLayer(nfeat, nhid, dropout=dropout, alpha=alpha, concat=True) for _ in range(nheads)]
+        for i, attention in enumerate(self.attentions):
+            self.add_module('attention_{}'.format(i), attention)
+
+        self.out_att = GraphAttentionLayer(nhid * nheads, nclass, dropout=dropout, alpha=alpha, concat=False)
+
+    def forward(self, x, adj):
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = torch.cat([att(x, adj) for att in self.attentions], dim=1)
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = F.elu(self.out_att(x, adj))
+        return F.log_softmax(x, dim=1)
+
+
+class SpGAT(nn.Module):
+    def __init__(self, nfeat, nhid, nclass, dropout, alpha, nheads):
+        """Sparse version of GAT."""
+        super(SpGAT, self).__init__()
+        self.dropout = dropout
+
+        self.attentions = [SpGraphAttentionLayer(nfeat, 
+                                                 nhid, 
+                                                 dropout=dropout, 
+                                                 alpha=alpha, 
+                                                 concat=True) for _ in range(nheads)]
+        for i, attention in enumerate(self.attentions):
+            self.add_module('attention_{}'.format(i), attention)
+
+        self.out_att = SpGraphAttentionLayer(nhid * nheads, 
+                                             nclass, 
+                                             dropout=dropout, 
+                                             alpha=alpha, 
+                                             concat=False)
+
+    def forward(self, x, adj):
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = torch.cat([att(x, adj) for att in self.attentions], dim=1)
+        x = F.dropout(x, self.dropout, training=self.training)
+        x = F.elu(self.out_att(x, adj))
+        return F.log_softmax(x, dim=1)
+
+
+class GraphSAGE(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers, dropout, return_embeds=False):
+        super(GraphSAGE, self).__init__()
+
+        self.convs = torch.nn.ModuleList()
+        self.convs.append(SAGEConv(input_dim, hidden_dim))
+        for _ in range(num_layers - 1):
+            self.convs.append(SAGEConv(hidden_dim, hidden_dim))
+
+        self.last_layer = SAGEConv(hidden_dim, output_dim)
+
+        self.bns = torch.nn.ModuleList([torch.nn.BatchNorm1d(hidden_dim) for _ in range(num_layers)])
+
+        self.softmax = torch.nn.LogSoftmax(dim=1)
+        self.dropout = dropout
+        self.return_embeds = return_embeds
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+        for bn in self.bns:
+            bn.reset_parameters()
+
+    def forward(self, x, edge_index):
+        for i, conv in enumerate(self.convs):
+            x = conv(x, edge_index)
+            x = self.bns[i](x)
+            x = F.relu(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+
+        x = self.last_layer(x, edge_index)
+
+        if not self.return_embeds:
+            x = self.softmax(x)
+
+        return x
+
+class GCN(torch.nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers,
+                 dropout, return_embeds=False):
+
+        super(GCN, self).__init__()
+
+        # A list of GCNConv layers
+        self.convs = [
+            GCNConv(input_dim, hidden_dim),
+        ]
+
+        for _ in range(num_layers-1):
+            self.convs.append(GCNConv(hidden_dim, hidden_dim))
+
+        self.last_layer = GCNConv(hidden_dim, output_dim)
+
+
+        # A list of 1D batch normalization layers
+        self.bns = []
+        for _ in range(num_layers):
+            self.bns.append(torch.nn.BatchNorm1d(hidden_dim))
+
+        self.convs = torch.nn.ModuleList(self.convs)
+        self.bns = torch.nn.ModuleList(self.bns)
+
+        # The log softmax layer
+        self.softmax = torch.nn.LogSoftmax()
+
+
+        # Probability of an element getting zeroed
+        self.dropout = dropout
+
+        # Skip classification layer and return node embeddings
+        self.return_embeds = return_embeds
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+        for bn in self.bns:
+            bn.reset_parameters()
+
+    def forward(self, x, adj_t):
+
+        for i in range(len(self.convs)):
+            x = self.convs[i](x, adj_t)
+            x = self.bns[i](x)
+            x = F.relu(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+
+        x = self.last_layer(x, adj_t)
+
+        if not self.return_embeds:
+            x = self.softmax(x)
+
+        return x
+
+#https://github.com/chennnM/GCNII/blob/master/model.py#L46    
+from torch.nn.parameter import Parameter
+
+class GraphConvolution(nn.Module):
+
+    def __init__(self, in_features, out_features, residual=False, variant=False):
+        super(GraphConvolution, self).__init__() 
+        self.variant = variant
+        if self.variant:
+            self.in_features = 2*in_features 
+        else:
+            self.in_features = in_features
+
+        self.out_features = out_features
+        self.residual = residual
+        self.weight = Parameter(torch.FloatTensor(self.in_features,self.out_features))
+        self.reset_parameters()
+
+    def reset_parameters(self):
+        stdv = 1. / math.sqrt(self.out_features)
+        self.weight.data.uniform_(-stdv, stdv)
+
+    def forward(self, input, adj , h0 , lamda, alpha, l):
+        theta = math.log(lamda/l+1)
+        hi = torch.spmm(adj, input)
+        if self.variant:
+            support = torch.cat([hi,h0],1)
+            r = (1-alpha)*hi+alpha*h0
+        else:
+            support = (1-alpha)*hi+alpha*h0
+            r = support
+        output = theta*torch.mm(support, self.weight)+(1-theta)*r
+        if self.residual:
+            output = output+input
+        return output
+
+class GCNII(nn.Module):
+    def __init__(self, nfeat, nlayers,nhidden, nclass, dropout, lamda, alpha, variant):
+        super(GCNII, self).__init__()
+        self.convs = nn.ModuleList()
+        for _ in range(nlayers):
+            self.convs.append(GraphConvolution(nhidden, nhidden,variant=variant))
+        self.fcs = nn.ModuleList()
+        self.fcs.append(nn.Linear(nfeat, nhidden))
+        self.fcs.append(nn.Linear(nhidden, nclass))
+        self.params1 = list(self.convs.parameters())
+        self.params2 = list(self.fcs.parameters())
+        self.act_fn = nn.ReLU()
+        self.dropout = dropout
+        self.alpha = alpha
+        self.lamda = lamda
+
+    def forward(self, x, adj):
+        _layers = []
+        x = F.dropout(x, self.dropout, training=self.training)
+        layer_inner = self.act_fn(self.fcs[0](x))
+        _layers.append(layer_inner)
+        for i,con in enumerate(self.convs):
+            layer_inner = F.dropout(layer_inner, self.dropout, training=self.training)
+            layer_inner = self.act_fn(con(layer_inner,adj,_layers[0],self.lamda,self.alpha,i+1))
+        layer_inner = F.dropout(layer_inner, self.dropout, training=self.training)
+        layer_inner = self.fcs[-1](layer_inner)
+        return F.log_softmax(layer_inner, dim=1)
+
+class GCNIIppi(nn.Module):
+    def __init__(self, nfeat, nlayers,nhidden, nclass, dropout, lamda, alpha,variant):
+        super(GCNIIppi, self).__init__()
+        self.convs = nn.ModuleList()
+        for _ in range(nlayers):
+            self.convs.append(GraphConvolution(nhidden, nhidden,variant=variant,residual=True))
+        self.fcs = nn.ModuleList()
+        self.fcs.append(nn.Linear(nfeat, nhidden))
+        self.fcs.append(nn.Linear(nhidden, nclass))
+        self.act_fn = nn.ReLU()
+        self.sig = nn.Sigmoid()
+        self.dropout = dropout
+        self.alpha = alpha
+        self.lamda = lamda
+
+    def forward(self, x, adj):
+        _layers = []
+        x = F.dropout(x, self.dropout, training=self.training)
+        layer_inner = self.act_fn(self.fcs[0](x))
+        _layers.append(layer_inner)
+        for i,con in enumerate(self.convs):
+            layer_inner = F.dropout(layer_inner, self.dropout, training=self.training)
+            layer_inner = self.act_fn(con(layer_inner,adj,_layers[0],self.lamda,self.alpha,i+1))
+        layer_inner = F.dropout(layer_inner, self.dropout, training=self.training)
+        layer_inner = self.sig(self.fcs[-1](layer_inner))
+        return layer_inner
